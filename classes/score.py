bee2549004626a8d70f87740a5a59125768b1f1d
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-07-09 22:45:59 -0300

    feat: ✨ add high score and set in file

--- classes/score.py
+++ classes/score.py
@@ -1,13 +1,17 @@
 from turtle import Turtle
+import os
 ALIGINMENT = "center"
 FONT = ("Courier", 24, "normal")
 GAME_OVER_MESSAGE = "FUCK YOU, LOSER"
+FILE_PATH = "./score.txt"
 
 class Score(Turtle):
 
     def __init__(self) -> None:
         super().__init__()
+        self.high_score = 0
         self.score = 0
+        self.get_high_score()
         self.color("white")
         self.print_score()
 
@@ -21,7 +25,7 @@ class Score(Turtle):
         self.clear()
         self.penup()
         self.goto(0, 260)
-        self.write(f"Score: {self.score}", align=ALIGINMENT, font=FONT)
+        self.write(f"Score: {self.score} | HIGH SCORE: {self.high_score}", align=ALIGINMENT, font=FONT)
         self.hideturtle()
 
 
@@ -29,3 +33,20 @@ class Score(Turtle):
         self.goto(0, 0)
         self.color("red")
         self.write(GAME_OVER_MESSAGE, align=ALIGINMENT, font=FONT)
+        self.set_high_score()
+
+
+    def get_high_score(self):
+        try:
+            with open(FILE_PATH, mode="r") as file:
+                for line in file.readlines():
+                    self.high_score = int(line)
+        except FileNotFoundError:
+            self.high_score = 0
+
+
+    def set_high_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open(FILE_PATH, mode="w") as file:
+                file.write(f"{self.high_score}")
